69c2defa45cabc5ce28f69f8534f5821fab3875a
Ankit Sharma <an2ki4t5s@gmail.com>
2023-12-01 12:09:03 +0530

    make register page running succesfull

--- app/urls.py
+++ app/urls.py
@@ -7,11 +7,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
         path('login/', login_view, name='login'),
+        path('register/', register, name='register'),
         path('user-profile/', user_profile, name='user_profile'),
 
 ]
 
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
-from .forms import LoginForm
+from .forms import *
+from django.contrib.auth.models import User
+
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = User.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            username=username
+        )
+        user.set_password(password)
+        user.save()
+        return redirect('/user-profile/')
+    return render(request,'register.html')
 
 def login_view(request):
     form = LoginForm(request.POST or None)
@@ -21,5 +40,11 @@ def login_view(request):
 
     return render(request, 'login.html', {'form': form})
 
+
+
+
+
 def user_profile(request):
+    
     return render(request, 'user.html')
+
